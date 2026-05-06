1c1f92bbc10211a5abc04c0a04380742437ad5b2
Dinesh choudhary <dc443228@gmail.com>
2026-05-06 15:13:25 +0530

    added index.css import in main.jsx

--- frontend_project/src/main.jsx
+++ frontend_project/src/main.jsx
@@ -6,6 +6,7 @@ import { AuthProvider } from './context/AuthContext'
 import { ThemeProvider } from './context/ThemeContext'
 import { ScanProvider } from './context/ScanContext'
 import App from './App.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
